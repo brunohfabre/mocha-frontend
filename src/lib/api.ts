90e905ca0324f457b99dc224822254076b5e0967
Bruno Fabre <bruno.hfabre@gmail.com>
2024-06-03 17:07:14 -0300

    add api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,38 @@
 import axios from 'axios'
 
 import { env } from '@/env'
+import { useAuth } from '@/stores/auth'
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
 })
+
+api.interceptors.request.use(
+  (config) => {
+    const token = useAuth.getState().token
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config
+  },
+  (error) => {
+    // Faz alguma coisa com o erro da requisição
+    return Promise.reject(error)
+  },
+)
+
+// Adiciona um interceptador na resposta
+api.interceptors.response.use(
+  (response) => {
+    // Qualquer código de status que dentro do limite de 2xx faz com que está função seja acionada
+    // Faz alguma coisa com os dados de resposta
+    return response
+  },
+  (error) => {
+    // Qualquer código de status que não esteja no limite do código 2xx faz com que está função seja acionada
+    // Faz alguma coisa com o erro da resposta
+    return Promise.reject(error)
+  },
+)
--- src/routes/_layouts/app.tsx
+++ src/routes/_layouts/app.tsx
@@ -1,12 +1,36 @@
+import { useEffect, useState } from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 
+import { api } from '@/lib/api'
 import { useAuth } from '@/stores/auth'
 
 export function AppLayout() {
   const token = useAuth((state) => state.token)
 
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    async function loadProfile() {
+      const response = await api.get('/profile')
+
+      console.log(response.data)
+    }
+
+    if (token) {
+      loadProfile()
+    }
+  }, [token])
+
   if (!token) {
     return <Navigate to="/sign-in" replace />
+  }
+
+  if (isLoading) {
+    return (
+      <div className="h-screen flex antialiased items-center justify-center">
+        <span className="animate-bounce">is loading...</span>
+      </div>
+    )
   }
 
   return (
